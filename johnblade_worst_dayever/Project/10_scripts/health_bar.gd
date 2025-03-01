fd3974cbd4d92277c0dfe2a1fc457e4e1929484d
dean <123314446+forerunnernerd@users.noreply.github.com>
2025-03-01 19:34:47 +0100

    UI Stuff

--- johnblade_worst_dayever/Project/10_scripts/health_bar.gd
+++ johnblade_worst_dayever/Project/10_scripts/health_bar.gd
@@ -3,5 +3,5 @@ extends TextureProgressBar
 func _ready() -> void:
 	pass
 	
-func _process(delta: float) -> void:
-	self.get_parent().value = Global.jhealth
+#func _process(delta: float) -> void:
+	#self.get_parent().value = Global.jhealth
