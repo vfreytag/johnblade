43ef927c5523c7ff5b672c7f953ba31dc95d3925
^v <163901228+vfreytag@users.noreply.github.com>
2025-03-01 21:06:30 +0100

    enemy

--- johnblade_worst_dayever/Project/10_scripts/basicenemy.gd
+++ johnblade_worst_dayever/Project/10_scripts/basicenemy.gd
@@ -9,9 +9,15 @@ func _init():
 	print ("enemyloaded")
 
 func _process(delta):
-		pass
+		if $RayCast3D.is_colliding():
+			attack()
+		else:
+			$AnimationTree/Stab.active = false
 
 func _on_area_3d_body_entered(CharacterBody3D):
 	#if (body.name == "CharacterBody3D"):
 		Global.jhealth -= 10
 		print (Global.jhealth)
+
+func attack():
+	$AnimationTree/Stab.active = true
