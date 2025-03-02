dc2cfe7fdaa880ea4ddf0f1fababb7fed0a7c23c
^v <163901228+vfreytag@users.noreply.github.com>
2025-03-02 15:23:30 +0100

    drops added and global edits

--- johnblade_worst_dayever/Project/10_scripts/global.gd
+++ johnblade_worst_dayever/Project/10_scripts/global.gd
@@ -2,14 +2,22 @@ extends Node
 
 #var jhealth: int = 100 
 var jhealth: int = 100
-var jblood: int = 50
+var jblood: int = 200
 var jcoffee: int = 50
 var jciggy: int  = 50
+
+var block = false
+
+var inv_choc: int
+var inv_meat: int
+var inv_cig: int
+var inv_coff: int
+
+var debt: int
+
 var crouchheight = 0.7
 var jump1height = 0.7
 var jump2height = 0.5
 var standheight = 1.1
-var block = false
-
 var jx = 0.575
 var jz = 0.262
