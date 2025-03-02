603a70b2119267ad79eaca975e3b7cf27391dc1f
^v <163901228+vfreytag@users.noreply.github.com>
2025-03-02 17:54:07 +0100

    consumer + controller

--- johnblade_worst_dayever/Project/10_scripts/player.gd
+++ johnblade_worst_dayever/Project/10_scripts/player.gd
@@ -1,19 +1,17 @@
-extends "res://Project/10_scripts/johnmovement.gd"
+class_name Player extends "res://Project/10_scripts/johnmovement.gd"
 
 var health = 100
+var chocheal = 25
 
 
 signal health_updated(health)
 signal killed() 
 signal hit
 
+
 func _init():
 	print(Global.jhealth)
 	Global.jhealth = 100
-
-func juan_hit():
-	print ("ow! my health is now at", Global.jhealth)
-	Global.jhealth -= 10
 	
 func _process(delta):
 	if Global.jhealth == 0:
@@ -23,6 +21,18 @@ func _on_body_entered(body):
 	hide()
 	hit.emit()
 	print ("ow!")
+
+
+func _input(event):
+	if event.is_action_pressed("consume_choc"):
+			consume_choc()
+	
+func consume_choc():
+	if Global.inv_choc > 0:
+		Global.inv_choc -= 1
+		Global.jhealth += chocheal
+	
+
 	
 #func start(pos):
 	#position = pos
